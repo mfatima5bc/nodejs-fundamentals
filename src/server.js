9330e8e36c0c92096b35581ddf34d4c8d0c359e3
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2023-01-13 16:19:35 -0300

    Finish crud operations

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import http from 'node:http'
+import json from './middleware/json.js'
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
+
+
+const server = http.createServer(async(req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
+
+    if (route) {
+        const routerParams = req.url.match(route.path)
+        
+        const { query, ...params } = routerParams.groups
+
+        req.params = params
+        req.query = query ? extractQueryParams(query) : {}
+
+        return route.handler(req, res)
+    }
+
+    return res.writeHead(404).end('ops!')
+})
+
+server.listen(3333, () => {
+    console.log('Server is running 🚀')
+})
